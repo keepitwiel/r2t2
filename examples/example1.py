155e05ba911d2f509baf4fa716ccd33f9c4c825c
Giel van de Wiel <giel.vandewiel@keepitwiel.local>
2025-02-04 16:56:37 +0100

    Add brightness feature

--- examples/example1.py
+++ examples/example1.py
@@ -6,7 +6,7 @@ from height import simplex_height_map
 
 
 def run(renderer: Renderer):
-    window = ti.ui.Window(name='Example 1', res=(1200, 600), fps_limit=60, pos=(0, 0))
+    window = ti.ui.Window(name='Example 1', res=(1200, 600), fps_limit=300, pos=(0, 0))
     gui = window.get_gui()
     canvas = window.get_canvas()
     out_image = np.zeros((1200, 600, 3), dtype=np.float32)
@@ -26,25 +26,26 @@ def run(renderer: Renderer):
         renderer.render()
 
         # show GUIs
-        with gui.sub_window("Camera", 0.5, 0.1, width=0.4, height=0.2):
+        with gui.sub_window("Camera", 0.5, 0.1, width=0.4, height=0.4):
             renderer.zoom = gui.slider_float("Zoom", renderer.zoom, 0.1, 10.0)
             renderer.x_offset = gui.slider_float("X offset", renderer.x_offset, -renderer.w_map, renderer.w_map)
             renderer.y_offset = gui.slider_float("Y offset", renderer.y_offset, -renderer.h_map, renderer.h_map)
             show_maxmipmap = gui.checkbox("Show MaxMipMap", show_maxmipmap)
             renderer.spp = gui.slider_int("Samples per pixel", renderer.spp, 1, 16)
+            renderer.brightness = gui.slider_float("Brightness", renderer.brightness, 0.1, 10.0)
 
-        with gui.sub_window("Algorithm", 0.5, 0.3, width=0.4, height=0.15):
+        with gui.sub_window("Algorithm", 0.5, 0.4, width=0.4, height=0.15):
             renderer.l_max = gui.slider_float("Maximum ray length", renderer.l_max, 0.0, l_max_max)
             renderer.random_xy = gui.checkbox("Randomize ray spawn point within pixel", renderer.random_xy)
 
-        with gui.sub_window("Sun", 0.5, 0.5, width=0.4, height=0.25):
+        with gui.sub_window("Sun", 0.5, 0.6, width=0.4, height=0.25):
             renderer.azimuth = gui.slider_float(f"Azimuth (degrees)", renderer.azimuth, 0, 360)
             azimuth_speed = gui.slider_float(f"Azimuth rotation speed (degrees per frame)", azimuth_speed, -5, 5)
             renderer.altitude = gui.slider_float("Altitude (degrees)", renderer.altitude, 0, 90)
             renderer.sun_radius = gui.slider_float("Sun radius (degrees)", renderer.sun_radius, 0.0, 5.0)
             renderer.sun_color = gui.color_edit_3("Color", renderer.sun_color)
 
-        with gui.sub_window("Sky", 0.5, 0.8, width=0.4, height=0.1):
+        with gui.sub_window("Sky", 0.5, 0.9, width=0.4, height=0.1):
             renderer.sky_color = gui.color_edit_3("Color", renderer.sky_color)
 
         # the following rotates the azimuth between 0 and 360 degrees, with increments of 1 degree per step
@@ -100,4 +101,4 @@ def main(n):
 
 if __name__ == "__main__":
     ti.init(arch=ti.vulkan)
-    main(n=512)
+    main(n=1024)
